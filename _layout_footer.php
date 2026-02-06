61d1bd544ac8e04d05c23ac6559c30a51a5d8e92
Elfira Risa <154104562+risaelfira@users.noreply.github.com>
2026-02-06 12:12:40 +0700

    Update footer copyright information and creator name

--- _layout_footer.php
+++ _layout_footer.php
@@ -1,4 +1,4 @@
   <footer class="text-center py-6 bg-gray-200 mt-10">
-    <p>&copy; 2025 AquascapeReef. Create by SAKARA - Samudera Karang</p>
+    <p>&copy; 2025 AquascapeReef. Create by ELFIRA RISA HIDAYAT</p>
     <p>Support by <a href="https://itpln.ac.id/" class="underline text-blue-600 hover:text-blue-800" target="_blank" rel="noopener noreferrer">ITPLN</a></p>
   </footer>
